295ac3eed808d8602af95c2de8e4c63bb8915dae
Mark Clanton <macmmc@marks-macbook-air.local>
2017-06-13 18:12:24 -0600

    Initial Commit

---
+++ FirstCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstCode
+//
+//  Created by Mark Clanton on 6/13/17.
+//  Copyright © 2017 Mark Clanton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
